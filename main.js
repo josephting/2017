9d7ab9c61ed4b757c4e906abf04905a16bd0508b
Joseph Ting <josephting90@gmail.com>
2016-12-31 00:40:04 +0900

    Add responsiveness

--- main.js
+++ main.js
@@ -2,12 +2,17 @@
   var resizeElements = function () {
     $('#container').css({
       'max-width': ($(window).height() * 0.95) * 0.67567567,
-      'height': $(window).height() * 0.95,
+      height: $(window).height() * 0.95,
       'margin-top': $(window).height() * 0.05 / 2,
     });
   };
 
-  resizeElements();
+  var initialization = function () {
+    $('#overlay').addClass('dismissed');
+    resizeElements();
+  };
+
+  initialization();
   $(window).on('resize', function () {
     resizeElements();
   });
