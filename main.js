8b0cc814c4356690eff62e17b77d0de8ea947fc2
Joseph Ting <josephting90@gmail.com>
2016-12-31 16:00:02 +0900

    Fix responsive trigger height
    Add query string name support

--- main.js
+++ main.js
@@ -8,8 +8,25 @@
   };
 
   var initialization = function () {
-    console.log(Modernizr.testAllProps('writingMode'));
-    if (!Modernizr.testAllProps('writingMode')) {
+    var nameSearch = new RegExp('[\\?&]n=([^&#]*)');
+    var name = nameSearch.exec(location.search || window.location.search);
+    if (name !== null) {
+      var names = decodeName(name[1]);
+      if (names !== false && isValidJson(names)) {
+        names = JSON.parse(names);
+        if (names.name_ja) {
+          names.name_ja = decodeURIComponent(names.name_ja);
+          $('#recipient').find('.name-ja').text(names.name_ja);
+        } else {
+          $('#recipient').find('.name-ja').parent().hide();
+        }
+
+        $('#recipient').find('.name').text(names.name);
+        $('#content').addClass('to');
+      }
+    }
+
+    if (!Modernizr.testAllProps('writingMode', 'vertical-rl')) {
       $('html').addClass('noWritingMode');
     }
 
@@ -18,10 +35,34 @@
   };
 
   var decodeName = function (str) {
+    if (!isValidBase64(str)) {
+      return false;
+    }
+
     return decodeURIComponent(Array.prototype.map.call(atob(str), function (c) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
   };
+
+  var isValidJson = function (str) {
+    try {
+      JSON.parse(str);
+    } catch (e) {
+      return false;
+    }
+
+    return true;
+  };
+
+  var isValidBase64 = function (str) {
+    try {
+      atob(str);
+    } catch (e) {
+      return false;
+    }
+
+    return true;
+  }
 
   initialization();
   $(window).on('resize', function () {
